a7f538e56068d19812b518041fae08fe940bd388
Afaan Naqvi <afaan.naqvi@gmail.com>
2021-08-16 12:44:09 +0200

    Update project with db version of thesaurus. Update config.ini accordingly

---
+++ 01_english_thesaurus/thesaurus_db_data.py
@@ -0,0 +1,49 @@
+from difflib import get_close_matches
+from logging import basicConfig
+import mysql.connector
+import configparser
+import logging
+
+
+def query_db(user_name, password, host, data_base):
+    userInput = input('Enter word: ')
+    userLowerInput = userInput.lower()
+    con = mysql.connector.connect(
+        user=user_name,
+        password=password,
+        host=host,
+        database=data_base
+        )
+
+    cursor = con.cursor()
+    query = "SELECT * FROM Dictionary WHERE Expression = '{}'".format(userLowerInput)
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    return results
+
+
+def main():
+    basicConfig(level=logging.INFO, format="%(asctime)s\t%(levelname)s\t%(message)s")
+
+    # Parsing Config File
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    USER_NAME = config['CREDS']['UserName']
+    PASSWORD = config['CREDS']['Password']
+    HOST = config['CREDS']['Host']
+    DATA_BASE = config['CREDS']['DataBase']
+
+    # Getting user input and appropriate response
+    appropriateResponse = query_db(USER_NAME, PASSWORD, HOST, DATA_BASE)
+
+    # Printing results or message
+    if not appropriateResponse:
+        print("No such word found")
+    else:
+        for i in appropriateResponse:
+            print(i[1])
+
+
+if __name__ == '__main__':
+    main()
