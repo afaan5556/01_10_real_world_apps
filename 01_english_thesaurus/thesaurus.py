102b07a3eff4685b0e63f9b0e0dbe4b755dd2a63
Afaan Naqvi <afaan.naqvi@gmail.com>
2021-08-16 11:56:53 +0200

    Update to petter print output

--- 01_english_thesaurus/thesaurus.py
+++ 01_english_thesaurus/thesaurus.py
@@ -1,38 +1,37 @@
-from difflib import SequenceMatcher, get_close_matches
-from logging import basicConfig, info
+from difflib import get_close_matches
+from logging import basicConfig
 import configparser
-import difflib
 import logging
 import json
 
 
 def read_json_data(full_data_path):
-	return json.load(open(full_data_path))
+    return json.load(open(full_data_path))
 
 
 def get_response(english_thesaurus_data, close_match_cutoff, close_match_count):
-	userInput = input('Enter word: ')
-	userLowerInput = userInput.lower()
-	# Case 1: Lower case match found
-	if userLowerInput in english_thesaurus_data.keys():
-		return english_thesaurus_data[userLowerInput]
+    userInput = input('Enter word: ')
+    userLowerInput = userInput.lower()
+    # Case 1: Lower case match found
+    if userLowerInput in english_thesaurus_data.keys():
+        return english_thesaurus_data[userLowerInput]
 
-	# Case 2: Close word match found in lower case
-	if not get_close_matches(userLowerInput, english_thesaurus_data.keys(), close_match_count, close_match_cutoff):
-			return 'No such word found. Please check entry'
-	else:
-		for i in get_close_matches(userLowerInput, english_thesaurus_data.keys(), close_match_count, close_match_cutoff):
-			while True:
-				userPromptResponse = input('Did you mean {}? Enter Y or N: '.format(i))
-				if userPromptResponse.lower() == 'y':
-					return english_thesaurus_data[i]
-				elif userPromptResponse.lower() == 'n':
-					break
-				else:
-					print('Invald response. Please enter either Y or N: ')
+    # Case 2: Close word match found in lower case
+    if not get_close_matches(userLowerInput, english_thesaurus_data.keys(), close_match_count, close_match_cutoff):
+        return 'No such word found. Please check entry'
+    else:
+        for i in get_close_matches(userLowerInput, english_thesaurus_data.keys(), close_match_count, close_match_cutoff):
+            while True:
+                userPromptResponse = input('Did you mean {}? Enter Y or N: '.format(i))
+                if userPromptResponse.lower() == 'y':
+                    return english_thesaurus_data[i]
+                elif userPromptResponse.lower() == 'n':
+                    break
+                else:
+                    print('Invald response. Please enter either Y or N: ')
 
-	# Case 3: No match found
-	return 'No such word found. Please check entry'
+    # Case 3: No match found
+    return 'No such word found. Please check entry'
 
 
 def main():
@@ -52,7 +51,12 @@ def main():
     # Getting user input and appropriate response
     appropriateResponse = get_response(englishThesaurusData, CLOSE_MATCH_CUTOFF, CLOSE_MATCH_COUNT)
 
-    print(appropriateResponse)
+    # Printing results or message
+    if isinstance(appropriateResponse, str):
+        print(appropriateResponse)
+    else:
+        for i in appropriateResponse:
+            print(i)
 
 
 if __name__ == '__main__':
